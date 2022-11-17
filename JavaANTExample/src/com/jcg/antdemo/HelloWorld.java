39d1a2b4786579813180556dd2a5a72e600b2836
sathish3131 <sathish.devops123@gmail.com>
2022-11-17 22:38:52 +0530

    sample code repo

---
+++ JavaANTExample/src/com/jcg/antdemo/HelloWorld.java
@@ -0,0 +1,11 @@
+
+package com.jcg.antdemo;
+
+
+public class HelloWorld
+{
+		public int add(int operand1, int operand2)
+    {
+		   return operand1+operand2;
+    }
+}
---
+++ JavaANTExample/src/com/jcg/antdemo/HelloWorldTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.jcg.antdemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest
+{
+
+		/**
+		 * Test method for {@link com.jcg.antdemo.HelloWorld#add(int, int)}.
+		 */
+		@Test
+		public void testAdd()
+		{
+				HelloWorld hw = new HelloWorld();
+				assertEquals(5, hw.add(3, 2));
+		}
+
+}
